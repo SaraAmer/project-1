82a21cdd2c7d530c5f24eb460421a4e45a903eea
Your Name <you@example.com>
2021-04-25 13:39:27 +0200

    This is Me

--- js/app.js
+++ js/app.js
@@ -26,35 +26,35 @@ function dynamicMenu(){
 
 function activeSection(){
     //get all of the anchors in the document
-  let links = document.querySelectorAll('a');
+   let links = document.querySelectorAll('a');
     //add n scroll event on the window to determine the active section 
-  window.addEventListener('scroll' , (e)=>{
-    for(section of sections){
+   window.addEventListener('scroll' , (e)=>{
+      for(section of sections){
         //get the position of the section
-      let position = section.getBoundingClientRect();
-      if(position.top>=0 && position.top < 250){
+         let position = section.getBoundingClientRect();
+         if(position.top>=0 && position.top < 250){
          //if the section is in the specific postion give it active class to be different
           section.classList.add('active-class');    
-          links.forEach((link)=>{
+           links.forEach((link)=>{
            //get the text of the anchor and if it's simillar to the active section change its background 
            //to make it active too
-           if(link.textContent===section.getAttribute('data-nav')){
-              link.style.background="blue"
-             }
-           else {
-              link.style.background="white"
+               if(link.textContent===section.getAttribute('data-nav')){
+                     link.style.background="blue"
+            }
+               else {
+                     link.style.background="white"
               }  
              }) 
     
             }
-        else {
-             section.classList.remove('active-class')
-           }
+         else {
+              section.classList.remove('active-class')
+            }
     
       
-        }
+         }
      
-        });
+         });
 }
 //calling the functions to excute
 dynamicMenu();
