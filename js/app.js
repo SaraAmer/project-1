29f3b6c77f7764929e94fe8c962a2d3eee7b2c13
Sara Amer <saraamersaad8@gmail.com>
2021-04-25 13:57:06 +0200

    final edit

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
    //select all sections in the document
 let sections = document.querySelectorAll('section');
 
-function dynamicMenu(){ 
+function createDynamicMenu(){ 
  //get the menue
   let nav = document.getElementById('navbar__list')
  //loop on the sections to add link by thier number
-  sections.forEach((section)=>{
+   sections.forEach((section)=>{
     //create new list
      let list = document.createElement('li') 
     //craete new anchor   
-     let link = document.createElement('a')
+     let anchors = document.createElement('a')
     //append the anchor to the list
-     list.appendChild(link)
+     list.appendChild(anchors)
     //get the attriibute on the section to name the anchor
      let data = section.getAttribute('data-nav')
      //set the data-nav to the text of the anchor
-     link.textContent=data
+     anchors.textContent=data
      //add on click action on the anchor to navigate to thr section
-     link.addEventListener('click' , (e)=>{
+     anchors.addEventListener('click' , (e)=>{
         section.scrollIntoView({"behavior": "smooth"});
-})
+     })
    nav.appendChild(list);
-});
+   });
 }
 
 function activeSection(){
     //get all of the anchor in the document
-   let links = document.querySelectorAll('a');
+   let anchors = document.querySelectorAll('a');
     //add n scroll event on the window to determine the active section 
    window.addEventListener('scroll' , (e)=>{
       for(section of sections){
@@ -35,27 +35,27 @@ function activeSection(){
          if(position.top>=0 && position.top < 250){
          //if the section is in the specific postion give it active class to be different
           section.classList.add('active-class');    
-           links.forEach((link)=>{
+           anchors.forEach((link)=>{
            //get the text of the anchor and if it's simillar to the active section change its background 
            //to make it active too
                if(link.textContent===section.getAttribute('data-nav')){
                      link.style.background="blue"
-            }
+               }
                else {
                      link.style.background="white"
-              }  
-             }) 
+               }  
+            }) 
     
-            }
+         }
          else {
               section.classList.remove('active-class')
-            }
+         }
     
       
-         }
+      }
      
-         });
+   });
 }
 //calling the functions to excute
-dynamicMenu();
+createDynamicMenu();
 activeSection();
